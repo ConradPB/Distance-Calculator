3ab8d87a6d14874beb5098d54d8fbd7a537504b4
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2025-07-11 00:14:50 +0300

    updated

---
+++ calculator/src/Calculator.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useCallback } from 'react';
+import { Container, TextField, Button, Typography, Box, Paper, CircularProgress } from '@mui/material';
+import './Calculator.css';
+
+function Calculator() {
+  ;
+}
+
+export default Calculator;
